c22f095813e7890ccfe1e9891b66d17d2ed0d4e3
musabjaved10 <musabjaved10@gmail.com>
2022-01-17 17:19:59 +0500

    live tracking

--- lib/screens/tracking/map_screen.dart
+++ lib/screens/tracking/map_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'service.dart';
 import 'package:location/location.dart';
 import 'package:project_pp/providers/map_provider.dart';
 import 'package:provider/provider.dart';
@@ -26,105 +28,23 @@ class _MapScreenState extends State<MapScreen> {
 
   late Set<Marker> markers = new Set();
   final List<Marker> loadedMarkers = [];
-   getPointMarkers() async{
-    final url = Uri.parse('https://point-pay-a430e-default-rtdb.firebaseio.com/NED.json');
-    try{
-      final response = await http.get(url);
-      // if(response.body == 'null'){
-      //   print('haha');
-      //   return;
-      // }
 
-      var extractedData = json.decode(response.body);
-      for(var i=1;i<extractedData.length;i++){
-        // print(myIcon);
-        loadedMarkers.add(Marker(
-              markerId: MarkerId(extractedData[i]['point_no'].toString()),
-              position: LatLng(extractedData[i]['lat'], extractedData[i]['long']), //position of marker
-              infoWindow: InfoWindow( //popup info
-                title: 'My Custom Title ',
-                snippet: 'My Custom Subtitle',
-              ),
-              icon: myIcon,
-            ));
-      }
-      markers=  loadedMarkers.toSet();
-      // print(markers);
-
-
-
-
-      // extractedData.forEach((id, markerData) {
-      //   print(markerData);
-      //   if(prodData !=null){
-      //   loadedMarkers.add(Marker(
-      //     markerId: MarkerId(extractedData['point_id']),
-      //     position: LatLng(24.8852602, 67.1774464), //position of marker
-      //     infoWindow: InfoWindow( //popup info
-      //       title: 'My Custom Title ',
-      //       snippet: 'My Custom Subtitle',
-      //     ),
-      //     ),
-      //     icon: BitmapDescriptor.defaultMarker,
-      //   ));
-      // };
-      // });
-    }catch(e){
-      print(e);
-      print('oops');
-    }
-  }
   @override
   void initState() {
     super.initState();
-
-    () async{
-      ByteData byteData = await DefaultAssetBundle.of(context).load("assets/icons/bus-icon.png");
-      Uint8List imageData = byteData.buffer.asUint8List();
-      myIcon = BitmapDescriptor.fromBytes(imageData);
-
-      // BitmapDescriptor.fromAssetImage(
-      //     ImageConfiguration(size: Size(12, 12)), 'assets/icons/bus-icon.png')
-      //     .then((onValue) {
-      //   myIcon = onValue;
-      // });
-      await getPointMarkers().then((value){
-        setState(() {
-          return;
-        });
-      });
-    }();
   }
+  @override
+  void didUpdateWidget(MapScreen oldWidget) {
 
+      setState((){
+       return;
+      });
 
-
-  // Location location = new Location();
-  //
-  // late bool _serviceEnabled;
-  // late PermissionStatus _permissionGranted;
-  // late LocationData _locationData;
-  //
-  // _serviceEnabled = await location.serviceEnabled();
-  // if (!_serviceEnabled) {
-  // _serviceEnabled = await location.requestService();
-  // if (!_serviceEnabled) {
-  // return;
-  // }
-  // }
-  //
-  // _permissionGranted = await location.hasPermission();
-  // if (_permissionGranted == PermissionStatus.denied) {
-  // _permissionGranted = await location.requestPermission();
-  // if (_permissionGranted != PermissionStatus.granted) {
-  // return;
-  // }
-  // }
-  //
-  // _locationData = await location.getLocation();
+    super.didUpdateWidget(oldWidget);
+  }
 
   late GoogleMapController mapController;
   final LatLng _center = const LatLng(24.8852602, 67.1774464);
-  late Marker marker;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -137,11 +57,26 @@ class _MapScreenState extends State<MapScreen> {
         title: const Text('Points Tracking'),
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(target: _center, zoom: 14.0),
-        markers: markers != null ? markers : Set.of([]),
-      ),
+      body: StreamBuilder(
+          stream: Service().markerStream(context),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if(snapshot.hasData){
+              return GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(target: _center, zoom: 14.0),
+                markers: snapshot.data,
+              );
+            }
+
+              return Center(child: CircularProgressIndicator(),);
+
+          }
+      )
+      // GoogleMap(
+      //   onMapCreated: _onMapCreated,
+      //   initialCameraPosition: CameraPosition(target: _center, zoom: 14.0),
+      //   markers: markers != null ? markers : Set.of([]),
+      // ),
     );
   }
 }
--- lib/screens/tracking/service.dart
+++ lib/screens/tracking/service.dart
@@ -1,23 +1,53 @@
-// class Service{
-//
-//   Future<Locations> getGoogleOffices() async {
-//     const googleLocationsURL = 'https://about.google/static/data/locations.json';
-//
-//     // Retrieve the locations of Google offices
-//     try {
-//       final response = await http.get(Uri.parse(googleLocationsURL));
-//       if (response.statusCode == 200) {
-//         return Locations.fromJson(json.decode(response.body));
-//       }
-//     } catch (e) {
-//       print(e);
-//     }
-//
-//     // Fallback for when the above HTTP request fails.
-//     return Locations.fromJson(
-//       json.decode(
-//         await rootBundle.loadString('assets/locations.json'),
-//       ),
-//     );
-//   }
-// }
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
+
+class Service{
+
+     getPointMarkers(BuildContext context) async{
+      final url = Uri.parse('https://point-pay-a430e-default-rtdb.firebaseio.com/NED.json');
+      ByteData byteData = await DefaultAssetBundle.of(context).load("assets/icons/bus-icon.png");
+      Uint8List imageData = byteData.buffer.asUint8List();
+      BitmapDescriptor myIcon = BitmapDescriptor.fromBytes(imageData);
+      final response = await http.get(url);
+
+        Set<Marker> markers = new Set();
+        List<Marker> loadedMarkers = [];
+        try{
+          var extractedData = json.decode(response.body);
+          for(var i=1;i<extractedData.length;i++){
+            // print(myIcon);
+            loadedMarkers.add(Marker(
+              markerId: MarkerId(extractedData[i]['point_no'].toString()),
+              position: LatLng(extractedData[i]['lat'], extractedData[i]['long']), //position of marker
+              infoWindow: InfoWindow( //popup info
+                title: "Point ${extractedData[i]['point_no']}",
+
+              ),
+              icon: myIcon,
+            ));
+          }
+          markers=  loadedMarkers.toSet();
+          return markers;
+
+        }catch(e){
+          print(e);
+          print('oops');
+        }
+
+    }
+
+     Stream<Set<Marker>> markerStream(context) async* {
+       while (true) {
+         await Future.delayed(Duration(milliseconds: 500));
+         Set<Marker> markers = await getPointMarkers(context);
+         print('hello');
+         yield markers;
+       }
+     }
+}
+
