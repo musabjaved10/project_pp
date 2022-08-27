664e947dbc4482e75e4f0d3b24ce75cacd47be59
musabjaved10 <musabjaved10@gmail.com>
2022-08-27 22:29:03 +0500

    payment histroy

--- lib/screens/payment/pay_view.dart
+++ lib/screens/payment/pay_view.dart
@@ -11,15 +11,17 @@ class PayView extends StatefulWidget {
 }
 
 class _PayViewState extends State<PayView> {
-
   bool isLoading = true;
   final qrController = Get.find<QRController>();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    ()async{
-
+    () async {
+      await qrController.getPaymentHistory();
+      setState(() {
+        isLoading = false;
+      });
     }();
   }
 
@@ -36,12 +38,142 @@ class _PayViewState extends State<PayView> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: isLoading ? Center(
-          child: SpinKitPulse(
-            color: Colors.white,
-            size: 65.0,
-          ),
-        ),
+        body: isLoading
+            ? Center(
+                child: SpinKitPulse(
+                  color: Colors.white,
+                  size: 65.0,
+                ),
+              )
+            : qrController.paymentHistory.isEmpty
+                ? Center(
+                    child: Text('You don\'t have any trnsaction history'),
+                  )
+                : SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: Get.height * 0.08,
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset(
+                            'assets/payment-history.jpg',
+                            fit: BoxFit.fill,
+                            height: Get.height * 0.25,
+                            width: Get.width * 0.9,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                              padding: EdgeInsets.symmetric(horizontal: 12),
+                              itemCount: qrController.paymentHistory.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  margin: EdgeInsets.only(bottom: 5),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(4)
+                                  ),
+                                  child: qrController.paymentHistory[index]['is_debit']
+                                      ? ListTile(
+                                    title: Text('Debit Transaction', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                                    minLeadingWidth: 0,
+                                    isThreeLine: true,
+                                    subtitle: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        RichText(
+                                          text:  TextSpan(text: 'Source: ',style: TextStyle(color: Colors.grey), children: [
+                                            TextSpan(
+                                                text: '${qrController.paymentHistory[index]['source']}',
+                                                style: TextStyle(fontStyle: FontStyle.italic)
+                                            )
+                                          ]),
+                                        ),
+                                        RichText(
+                                          text:  TextSpan(text: 'Time: ',style: TextStyle(color: Colors.grey), children: [
+                                            TextSpan(
+                                                text: '28 August, 2022',
+                                                style: TextStyle(fontStyle: FontStyle.italic)
+                                            )
+                                          ]),
+                                        )
+                                      ],
+                                    ),
+                                    leading: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.arrow_circle_down,
+                                          color: Colors.red,
+                                        ),
+                                      ],
+                                    ),
+                                    trailing: Text(
+                                      '${qrController.paymentHistory[index]['amount']}/-',
+                                      style: TextStyle(color: Colors.red, fontSize: 16),
+                                    ),
+                                  )
+                                      : ListTile(
+                                    title: Text('Debit Transaction', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                                    minLeadingWidth: 0,
+                                    isThreeLine: true,
+                                    subtitle: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        RichText(
+                                          text:  TextSpan(text: 'Source: ',style: TextStyle(color: Colors.grey), children: [
+                                            TextSpan(
+                                                text: '${qrController.paymentHistory[index]['source']}',
+                                                style: TextStyle(fontStyle: FontStyle.italic)
+                                            )
+                                          ]),
+                                        ),
+                                        RichText(
+                                          text:  TextSpan(text: 'Time: ',style: TextStyle(color: Colors.grey), children: [
+                                            TextSpan(
+                                                text: '28 August, 2022',
+                                                style: TextStyle(fontStyle: FontStyle.italic)
+                                            )
+                                          ]),
+                                        )
+                                      ],
+                                    ),
+                                    leading: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.arrow_circle_up,
+                                          color: Colors.green,
+                                        ),
+                                      ],
+                                    ),
+                                    trailing: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          '${qrController.paymentHistory[index]['amount']}/-',
+                                          style: TextStyle(color: Colors.green, fontSize: 16),
+                                        ),
+                                        SizedBox(height: 8,),
+                                        Text(
+                                          'View Point Card >',
+                                          style: TextStyle(color: Colors.green, fontSize: 12, fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                );
+                              }),
+                        )
+                      ],
+                    ),
+                  ),
       ),
     );
   }
